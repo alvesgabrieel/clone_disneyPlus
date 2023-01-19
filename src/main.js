02a6d2a1c1ddbf60eb295402ca6cd7de249bb156
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-01-19 19:03:35 -0300

    add faq session

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-//Lógica p/ mudar os titulos sessao 'shows'
-
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -13,7 +12,13 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i= 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+//funções
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -28,4 +33,12 @@ function escondeTodasAbas() {
     for (let i = 0;  i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log(elemento)
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
 }
